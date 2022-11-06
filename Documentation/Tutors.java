3078f9132d9e45c42674f185b5df484b885ddbcc
Savit10 <savita@my.ofs.edu.sg>
2022-11-06 16:34:45 +0800

    g

---
+++ Documentation/Tutors.java
@@ -0,0 +1,14 @@
+import java.util.ArrayList;
+
+public Tutors {
+    // attributes
+    private String firstName;
+    private String lastname;
+    private int gradeLevel;
+    private ArrayList <String> subjectsTaught = new ArrayList<String>();
+    private boolean [] sessionsAvailable;
+
+
+
+
+}
